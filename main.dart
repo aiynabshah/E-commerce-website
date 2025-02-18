15f92292516e6428aee60b903b7cf3ca950f57a1
aiynab shah <Aiynabshah81992@gmail.com>
2025-02-18 00:50:38 -0800

    Update main.dart
    connect with database also

--- main.dart
+++ main.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'loginpage.dart'; // Import the login screen
+import 'loginpage.dart'; 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb)
+  { await  Firebase.initializeApp(options:FirebaseOptions( apiKey: "AIzaSyCwrbKa8tDIdg_Mtj9gCr0K-giS8wIwgYA",
+  authDomain: "e-commerce-26ea3.firebaseapp.com",
+  projectId: "e-commerce-26ea3",
+  storageBucket: "e-commerce-26ea3.firebasestorage.app",
+  messagingSenderId: "672814674603",
+  appId: "1:672814674603:web:b86bb36c57995c20653dee"));
+
+  }
+ else{
+  await  Firebase.initializeApp();
+ }
   runApp(const MyApp());
 }
 
@@ -20,3 +35,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
